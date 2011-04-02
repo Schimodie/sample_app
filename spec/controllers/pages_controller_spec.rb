e2b82d3baf5f1160325f6b6464dfb11100c9c427
Schimodie <radu.gagos@gmail.com>
2011-04-03 00:19:42 +0300

    Help tests pass

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +15,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", 
+                                    :content => @base_title + "Home")
     end
   end
 
@@ -23,7 +28,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", 
+                                    :content => @base_title + "Contact")
     end
   end
 
@@ -35,10 +41,11 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", 
+                                    :content => @base_title + "About")
     end
   end
-
+  
   describe "GET 'help'" do
     it "should be succesful" do
       get 'help'
@@ -47,7 +54,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+      response.should have_selector("title", 
+                                    :content => @base_title + "Help")
     end
   end
 end
